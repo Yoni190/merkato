41d8f7d91f65c7365464dd32cbb2d00280398a8b
yoni <yfisuhu@gmail.com>
2026-02-26 14:50:40 +0300

    Add login function

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -5,14 +5,16 @@ exports.register = async (req, res) => {
     try {
         const { f_name, l_name, email, password, role } = req.body
 
+        if(!f_name || !l_name || !email || !password || !role) {
+            return res.status(400).json({ error: 'Missing Data!' })
+        }
+
         const existingUser = await User.findOne({ email })
         if(existingUser) {
             return res.status(400).json({ error: 'Email already exists' })
         }
 
-        if(!f_name || !l_name || !email || !password || !role) {
-            return res.status(400).json({ error: 'Missing Data!' })
-        }
+        
 
         const hashedPassword = await bcrypt.hash(password, 10)
 
@@ -25,6 +27,30 @@ exports.register = async (req, res) => {
         })
 
         return res.status(200).json({ message: 'User registered successfully!' })
+    } catch (error) {
+        return res.status(500).json({ error: `Error: ${error}` })
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({ error: 'Missing Data!' })
+        }
+
+        const user = await User.findOne({ email })
+        if(!user) {
+            return res.status(400).json({ error: 'User not registered' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch) {
+            return res.status(400).json({ error: 'Wrong Credentials' })
+        }
+
+        return res.status(200).json({ message: 'User logged in successfully!' })
     } catch (error) {
         return res.status(500).json({ error: `Error: ${error}` })
     }
--- server/routes/route.js
+++ server/routes/route.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/UserController.js')
 const router = express.Router()
 
 router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 router.get('/test', (req, res) => {
     return res.json({ message: 'Test was successful'})
