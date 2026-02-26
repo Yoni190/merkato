2278bfb484a6ed6f9df4a7b4372c5b3f15d6e7fa
yoni <yfisuhu@gmail.com>
2026-02-26 14:08:02 +0300

    Create category model

---
+++ server/models/CategoryModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const CategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product'}
+})
