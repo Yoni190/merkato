7e0aa175282333c4a71a82fb3b0389ae6796ccd6
yoni <yfisuhu@gmail.com>
2026-02-26 14:28:10 +0300

    Log mongodb errors

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const server = express()
 mongoose.connect('mongodb://127.0.0.1:27017/merkato')
         .then(() => console.log('MongoDB connected!'))
+        .catch((err) => console.log(err))
 
 
 server.listen(5000, () => console.log('Server started at port 5000'))
