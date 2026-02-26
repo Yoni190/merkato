27dd65fa94c2eca0484985c9372649b26261f18f
yoni <yfisuhu@gmail.com>
2026-02-26 13:54:45 +0300

    Make user columns required

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema
 
-const User = new Schema({
-    f_name: String,
-    l_name: String,
-    email: String,
-    password: String,
-    role: Number
+const UserSchema = new mongoose.Schema({
+    f_name: {
+        type: String,
+        required: true,
+    },
+    l_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: Number,
+        required: true,
+    },
 })
+
+module.exports = mongoose.model('User', UserSchema)
